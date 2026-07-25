1d8c576d9ff2c3794866bfc0365c9b313a366b97
Ramesh Naandi <rameshvai340@gmail.com>
2026-07-26 00:14:31 +0530

    16

--- app/layout.tsx
+++ app/layout.tsx
@@ -127,7 +127,53 @@ export default function RootLayout({
     <html lang="en" className="bg-white">
 
 
+      <head>
+
+        {/* Google Tag Manager */}
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),
+              dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KQV23QMH');
+            `,
+          }}
+        />
+
+        {/* End Google Tag Manager */}
+
+      </head>
+
+
+
       <body className="font-sans antialiased bg-white relative">
+
+
+        {/* Google Tag Manager (noscript) */}
+
+        <noscript>
+
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KQV23QMH"
+            height="0"
+            width="0"
+            style={{
+              display: 'none',
+              visibility: 'hidden'
+            }}
+          />
+
+        </noscript>
+
+        {/* End Google Tag Manager (noscript) */}
+
 
 
         {/* Background Video */}
